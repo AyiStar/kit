6c77863313e0fbaf820e3c88a56894c02e3d3aec
AyiStar <ayistar@outlook.com>
2024-01-25 23:03:00 +0800

    start type construction

--- kitten/core/data_type.h
+++ kitten/core/data_type.h
@@ -1,5 +1,21 @@
 #pragma once
 
 namespace kitten {
-enum class DataType { INT16, INT32, FLOAT32, FLOAT16 };
-}
+enum class DataType : uint16_t { INT16, INT32, FLOAT32, FLOAT16 };
+
+class TypeMetaData final {
+public:
+  DataType data_type();
+
+private:
+};
+
+class TypeMeta final {
+public:
+private:
+  uint16_t index_;
+};
+
+template <typename T> TypeMeta make_type_meta();
+
+} // namespace kitten
